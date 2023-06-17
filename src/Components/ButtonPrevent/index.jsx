9bd3d94ef8ae978fc25eb754c0366934ac3e1430
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-17 01:28:59 -0300

    se elimino el componente buttonPrevent y se modifico login

--- src/Components/ButtonPrevent/index.jsx
+++
@@ -1,7 +0,0 @@
-import ImgPrevent from '../../assets/Img/bottonPrevent.svg';
-
-const BottonPrevent = (destino)=>{
-        return <img src={ImgPrevent} alt="Prevent" onClick={destino} className='m-5' />
-}
-
-export default BottonPrevent;
