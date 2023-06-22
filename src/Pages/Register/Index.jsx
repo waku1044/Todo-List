bc3e1095d7b189b85ee98cdfd97a0250cdbf6a2c
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-06-22 02:39:40 -0300

    se creo la funcionalidad del login con sus respectivos errores

--- src/Pages/Register/Index.jsx
+++ src/Pages/Register/Index.jsx
@@ -1,29 +1,100 @@
 import './style.css';
-import { BtnLink } from '../../Components/BtnLink';
 import { Link } from 'react-router-dom';
-// import closeEye from '../../assets/Img/closeEye.svg';
-import imgPrevent from '../../assets/Img/bottonPrevent.svg'
+import { useState } from 'react';
+import imgPrevent from '../../assets/Img/bottonPrevent.svg';
+import { useNavigate } from 'react-router-dom';
+
+const regisUser = {
+    userName:'',
+    userPassword:'',
+    userPassRepeat:''
+}
+const Register = ()=>{
 
 
-const Login = ()=>{
 
+    const [dataUser, setDataUser] = useState(regisUser);
+    const [error, setError] = useState({});
+    const navegate = useNavigate();
+    
+    const handleChange = (e)=>{
+        setDataUser({...dataUser,[e.target.name]:e.target.value})
+    }
+    const validate = ()=>{
+        let errorList = {};
 
-    return <div className="register bg-dark ">
-        <Link to='/' ><img src={imgPrevent} alt="" className='' /></Link>
-        <h2 className='text-white ms-5 mt-2'>Register</h2>
-        <form action="" className='mt-5 d-flex flex-column justify-content-around gap-1 w-75 mx-auto'>
-            <label htmlFor="usename" className='text-white'>Username</label>
-            <input type="text" name='useranme' placeholder='Enter your Username' className='mb-5'/>
+        if(!dataUser.userName)
+        errorList = {...errorList, user:'Campo obligatorio.'};
+        else if(dataUser.userName.length < 4)
+        errorList = {...errorList, user:'Debe tener al menos 4 digitos.'};
+        if(!dataUser.userPassword)
+        errorList = {...errorList, password:'Campo obligatorio.'}
+        else if(dataUser.userPassword.length < 6)
+        errorList = {...errorList, password:'Debe tener al menos 6 digitos.'};
+        if(!dataUser.userPassRepeat)
+        errorList = {...errorList, passRepeat:'Campo obligatorio.'}
+        else if(dataUser.userPassword != dataUser.userPassRepeat)
+        errorList = {...errorList, passRepeat:'No coincide con la password introducida.'}
 
-            <label htmlFor="usepassword" className='text-white'>Password</label>
-            <input type="password" name='userpassword' placeholder='Enter your Password' className='mb-5 '/>
+        return errorList;
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        let errores = validate();
+        if(Object.keys(errores).length < 1){
+            console.log(dataUser)
+            localStorage.setItem('user', dataUser.userName);
+            localStorage.setItem('password', dataUser.userPassword);
+            navegate('/login')
+            console.log('Puede ingresar')
+        }
+        setError(errores)
+        
+    }
 
-            <label htmlFor="usepassword" className='text-white'>Repeat Password</label>
-            <input type="password" name='userpassword' placeholder='Repeat Password' className='mb-5 '/>
+    return <div className="register bg-dark pt-5">
+        <Link to='/' ><img src={imgPrevent} alt="" className='ms-4 mb-4' /></Link>
+        <h2 className='text-white ms-5 mt-2'>Registro</h2>
+        <form action="" className='mt-5 d-flex flex-column justify-content-around gap-1 w-75 mx-auto' onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="usename" className='text-white'>Usuario</label>
+                <input type="text" 
+                name='userName' 
+                placeholder='Minimo 4 digitos' 
+                className=''
+                value={dataUser.userName}
+                onChange={handleChange}
+                />
+                {error.user && <p className='errorText'>{error.user}</p>}
+            </div>
+            <div>
+                <label htmlFor="usepassword" className='text-white'>Password</label>
+                <input type="password" 
+                name='userPassword' 
+                placeholder='Minimo 6 digitos' 
+                className=''
+                value={dataUser.userPassword}
+                onChange={handleChange}
+                />
+                {error.password && <p className='errorText'>{error.password}</p>}
 
-            <BtnLink bg='#8687E7' width='75%' className='btn mx-auto'>Login</BtnLink>
+            </div>
+            <div>
+                <label htmlFor="usepassword" className='text-white'>Repetir Password</label>
+                <input type="password" 
+                name='userPassRepeat' 
+                placeholder='Repetir Password' 
+                className=''
+                value={dataUser.userPassRepeat}
+                onChange={handleChange}
+                />
+                {error.passRepeat && <p className='errorText'>{error.passRepeat}</p>}
+
+            </div>
+
+            <button type='submit' className='btn btn-primary mx-auto'>Login</button>
         </form>
     </div>
 }
 
-export default Login;
+export default Register;
