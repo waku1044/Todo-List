edf75469317e5c3e999e44775a121082c5eef0d8
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-10-22 23:14:32 -0300

    se mejor maquetado movil

--- src/Components/HeaderHome/index.jsx
+++ src/Components/HeaderHome/index.jsx
@@ -4,7 +4,7 @@ import ImgUser from '../../assets/Img/imgUserMale.png';
 const HeaderHome = ()=>{
     
     
-    return <header className='d-flex justify-content-between align-items-center px-5 bg-secondary '>
+    return <header className='d-flex w-100  justify-content-between align-items-center px-5 bg-secondary '>
         <h5 className='fw-bold'>Inicio</h5>
         <h2 className='fst-italic text-capitalize'>{localStorage.getItem('user')}</h2>
         <img src={ImgUser} alt="User" />
