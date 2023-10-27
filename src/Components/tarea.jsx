6982035c408b453bec901baf41b8d48613b6ed41
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2023-10-27 03:13:37 -0300

    se dio margen a los botones de la tarea

--- src/Components/tarea.jsx
+++ src/Components/tarea.jsx
@@ -14,12 +14,12 @@ const tarea = ({datos})=>{
     </div>
     <div className="card-body pt-0">
       <h5 className="card-title fw-bolder text-warning"><b>{datos.titulo}</b></h5>
-      <p className="card-text fw-semibold text-dark"><b>{datos.description}</b></p>
+      <p className="card-text fw-semibold text-dark h3"><b>{datos.description}</b></p>
       <p className="card-text">Día: <b>{datos.day}</b></p>
       <p className="card-text">Hora: <b>{datos.time}</b></p>
-      <div className="card-footer d-flex justify-content-around">
-      <Link to={`/edit?id=${datos.id}`} className="btn btn-primary">Editar</Link>
-      <button className="btn-danger btn " onClick={() => eliminarTarea(datos.id)} >Eliminar</button>
+      <div className="">
+      <Link to={`/edit?id=${datos.id}`} className="btn btn-primary me-3 rounded">Editar</Link>
+      <button className="btn-danger btn rounded" onClick={() => eliminarTarea(datos.id)} >Eliminar</button>
 
       </div>
     </div>
